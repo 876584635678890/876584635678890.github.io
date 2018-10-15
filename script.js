e2234fa6215ed9f2c2c34d53e03714e402affc7f
876584635678890 <43884492+876584635678890@users.noreply.github.com>
2018-10-16 01:35:36 +0200

    Improved

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ with (new WebSocket('wss://play-worker.c1games.com/')) {
     send('n[{"game_to_client":0,"player_1_err":1,"player_2_err":2,"game_over":3},{"algo_zip":0},"%7B%22player_1%22%3A%22manual%22%2C%22player_2%22%3A%22manual%22%7D"]')
   }
   onmessage = function (event) {
-    if (!event.data.includes('debug'))
-        return show('waiting..')
+    if (!event.data.includes('debug')) return show('waiting..')
     close()
     show(JSON.stringify(JSON.parse(`{"de${event.data.split('{"de')[1]}`), null, 2))
   }
